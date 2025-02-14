e418b73e20483537553ec441232290ca0230d892
anikatasnin <jarintasnin09@gmail.com>
2025-02-14 22:09:49 +0600

    added footer

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,9 +1,11 @@
+import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
 const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 };
