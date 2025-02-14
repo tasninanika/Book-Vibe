645491136bd29600a8ea034cd43f7fefa5f1dd9a
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 00:35:00 +0600

    updated books.jsx

---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,30 @@
+const Books = () => {
+  return (
+    <div>
+      <div>
+        <h2 className="text-5xl">Books</h2>
+      </div>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure className="px-10 pt-10">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+            className="rounded-xl"
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+          <div className="card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Books;
