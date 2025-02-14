642b681cf0bd67bb7ae5f8bc7ecf0ad9db3ea13c
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 00:41:07 +0600

    updated books.jsx

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,27 +1,17 @@
+import { useEffect, useState } from "react";
+
 const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("./booksData.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
   return (
     <div>
       <div>
         <h2 className="text-5xl">Books</h2>
-      </div>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure className="px-10 pt-10">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-            className="rounded-xl"
-          />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
       </div>
     </div>
   );
