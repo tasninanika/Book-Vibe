c0e7b80b1a4acb272ad574d23d91b9cddcbb0d0c
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 01:00:59 +0600

    updated books.jsx

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -4,9 +4,10 @@ const Book = ({ book }) => {
     <div>
       <div className="card bg-base-100 w-96 shadow-sm">
         <figure className="px-10 pt-10">
-          <img src={image} alt="Shoes" className="rounded-xl" />
+          <img src={image} alt="Book" className="rounded-xl h-[166px] " />
         </figure>
         <div className="card-body items-center text-center">
+          <button className="btn btn-sm">Small</button>
           <h2 className="card-title">{bookName}</h2>
           <p>{author}</p>
         </div>
