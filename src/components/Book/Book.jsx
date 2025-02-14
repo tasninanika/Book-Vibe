adf91635049b14e4a0227f4eedf90eba1ad35b88
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 01:10:50 +0600

    updated books.jsx

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,22 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Book = ({ book }) => {
-  const { image, bookName, author, rating, category, tags } = book;
+  const { bookId, image, bookName, author, rating, category, tags } = book;
   return (
-    <div>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure className="px-10 pt-10">
-          <img src={image} alt="Book" className="rounded-xl h-[166px] " />
-        </figure>
-        <div className="card-body items-center text-center">
-          <div className="flex justify-center gap-4">
-            {tags.map((tag) => (
-              <button className="btn btn-sm">{tag}</button>
-            ))}
+    <Link to={`/books/${bookId}`}>
+      <div>
+        <div className="card bg-base-100 w-96 shadow-sm">
+          <figure className="px-10 pt-10">
+            <img src={image} alt="Book" className="rounded-xl h-[166px] " />
+          </figure>
+          <div className="card-body items-center text-center">
+            <div className="flex justify-center gap-4">
+              {tags.map((tag) => (
+                <button className="btn btn-sm">{tag}</button>
+              ))}
+            </div>
+            <h2 className="card-title">{bookName}</h2>
+            <p>{author}</p>
           </div>
-          <h2 className="card-title">{bookName}</h2>
-          <p>{author}</p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
