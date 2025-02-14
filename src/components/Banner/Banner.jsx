fdc9c0582abfadb5f9f25fda2058cbbc321d9258
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 00:29:30 +0600

    updated navbar.jsx

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import bannerImg from "../../assets/banner-book.png";
+const Banner = () => {
+  return (
+    <div>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={bannerImg} className="max-w-sm rounded-lg" />
+          <div>
+            <h1 className="text-5xl font-bold">
+              Books to freshen up your bookshelf
+            </h1>
+            <button className="btn btn-primary">View The List</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
+import Banner from "../Banner/Banner";
+
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      <Banner></Banner>
     </div>
   );
 };
