21debec8c052af6f0a5edf954f0193dbf3136301
anikatasnin <jarintasnin09@gmail.com>
2025-02-14 23:00:09 +0600

    added footer

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
@@ -5,6 +6,7 @@ const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Outlet></Outlet>
       <Footer></Footer>
     </div>
   );
