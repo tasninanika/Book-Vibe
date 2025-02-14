48815d1a368211c3c19d032f839309a60ac6e4a7
anikatasnin <jarintasnin09@gmail.com>
2025-02-14 23:06:57 +0600

    added error.jsx

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,10 @@
+const Error = () => {
+  return (
+    <div>
+      <h1 className="text-5xl">Oops!</h1>
+      <p className="text-lg">Sorry, an unexpected error has occurred.</p>
+    </div>
+  );
+};
+
+export default Error;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./components/Root/Root";
+import Error from "./components/Error/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <Error></Error>,
   },
 ]);
 
