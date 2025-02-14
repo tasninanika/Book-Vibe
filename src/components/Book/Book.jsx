79ee31e25815a24c93fff91833af4dafabe8616c
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 00:55:44 +0600

    updated books.jsx

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,6 +1,5 @@
 const Book = ({ book }) => {
   const { image, bookName, author, rating, category, tags } = book;
-  console.log(book);
   return (
     <div>
       <h2>books</h2>
@@ -9,14 +8,8 @@ const Book = ({ book }) => {
           <img src={image} alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+          <h2 className="card-title">{bookName}</h2>
+          <p>{author}</p>
         </div>
       </div>
     </div>
