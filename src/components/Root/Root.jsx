5fe7a46fe5d3f51577267b8e1559b0fbe4fed1b0
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 00:06:38 +0600

    updated navbar.jsx

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -4,7 +4,7 @@ import Navbar from "../Navbar/Navbar";
 
 const Root = () => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
