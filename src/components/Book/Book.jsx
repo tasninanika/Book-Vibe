4a16fca977aa8cc4d643f3a0606c903310c4a8c7
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 00:53:48 +0600

    updated books.jsx

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,15 +1,12 @@
 const Book = ({ book }) => {
-  const { bookName, author, image, rating, category, tags } = book;
+  const { image, bookName, author, rating, category, tags } = book;
+  console.log(book);
   return (
     <div>
       <h2>books</h2>
       <div className="card bg-base-100 w-96 shadow-sm">
         <figure className="px-10 pt-10">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-            className="rounded-xl"
-          />
+          <img src={image} alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">Card Title</h2>
