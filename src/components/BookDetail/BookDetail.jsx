956ce6e94e32c7ac4169f66e90211842c8998fe2
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 14:25:06 +0600

    updated bookdetail.jsx

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -6,7 +6,6 @@ const BookDetail = () => {
   const data = useLoaderData();
   const book = data.find((book) => book.bookId === id);
   const {
-    bookId,
     image,
     bookName,
     author,
@@ -20,7 +19,25 @@ const BookDetail = () => {
   } = book;
   return (
     <div>
-      <h2>Book detail</h2>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row gap-10">
+          <div className="bg-base-300 w-[573px] rounded-xl justify-center">
+            <img
+              src={image}
+              className="max-w-sm rounded-lg mx-auto w-[425px] h-[564px] p-10"
+            />
+          </div>
+          <div className="flex-1">
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>{" "}
     </div>
   );
 };
