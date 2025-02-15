23b3b34a67fe32fc40693936cd0c9767b181964b
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 12:50:54 +0600

    updated books.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,15 +3,17 @@ const Banner = () => {
   return (
     <div>
       <div className="hero min-h-screen bg-gray-100 rounded-2xl my-12">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="flex-1">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-40">
+          <div className="">
             <img src={bannerImg} className="max-w-sm rounded-lg" />
           </div>
-          <div className="flex-1">
+          <div className="w-[526px] h-[168px]">
             <h1 className="text-[56px] font-bold">
               Books to freshen up your bookshelf
             </h1>
-            <button className="btn btn-primary">View The List</button>
+            <button className="btn text-lg font-bold py-6 px-6 rounded-md bg-[#23BE0A] mt-10 text-white">
+              View The List
+            </button>
           </div>
         </div>
       </div>
