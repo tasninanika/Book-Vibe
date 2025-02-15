91e0fe5e942766d9026616e22e8c646de74e73d2
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 12:53:38 +0600

    updated books.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import bannerImg from "../../assets/banner-book.png";
 const Banner = () => {
   return (
     <div>
-      <div className="hero min-h-screen bg-gray-100 rounded-2xl my-12">
+      <div className="hero min-h-screen bg-gray-50 rounded-2xl my-12">
         <div className="hero-content flex-col lg:flex-row-reverse gap-40">
           <div className="">
             <img src={bannerImg} className="max-w-sm rounded-lg" />
