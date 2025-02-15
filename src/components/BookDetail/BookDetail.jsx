fd992926a708329fc9c2845c7ea7e3fe64fc0bb9
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 15:42:24 +0600

    updated bookdetail.jsx

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -19,50 +19,56 @@ const BookDetail = () => {
   } = book;
   return (
     <div>
-      <div className="hero min-h-screen">
+      <div className="hero min-h-screen my-20">
         <div className="hero-content flex-col lg:flex-row gap-10">
-          <div className="bg-base-300 w-[573px] h-[711px] rounded-xl justify-center">
+          <div className="bg-base-300 w-[573px] h-[680px] rounded-xl flex items-center justify-center">
             <img
               src={image}
-              className="max-w-sm rounded-lg mx-auto w-[425px] h-[564px] items-center justify-center flex"
+              alt="Book"
+              className="w-[425px] h-[564px] rounded-lg object-contain"
             />
           </div>
+
           <div className="flex-1 mx-auto">
-            <h2 className="text-5xl font-bold">{bookName}</h2>
-            <p className="text-[20px] border-gray-200 border-b py-2">
-              By : {author}
-            </p>
-            <p className="py-2 border-gray-200 border-b">{category}</p>
-            <p className="py-2 text-gray-600">
-              <span className="font-bold text-black">Review :</span> {review}
-            </p>
-            <p className="pb-2 border-gray-200 border-b">
-              <span className="pr-5 font-bold">Tag</span>
-              {tags.map((tag, index) => (
-                <button
-                  key={index}
-                  className="btn btn-sm rounded-full px-6 border-none bg-green-50 mr-4 text-[#23BE0A] text-lg"
-                >
-                  #{tag}
-                </button>
-              ))}
-            </p>
-            <p className="pb-1 pt-4 text-gray-600">
-              <span className="font-bold text-black">Number of Pages:</span>{" "}
-              {totalPages}
-            </p>
-            <p className="py-1 text-gray-600">
-              <span className="font-bold text-black">Publisher:</span>{" "}
-              {publisher}
-            </p>
-            <p className="py-1 text-gray-600">
-              <span className="font-bold text-black">Year of Publishing:</span>{" "}
-              {yearOfPublishing}
-            </p>
-            <p className="py-1 text-gray-600">
-              <span className="font-bold text-black">Rating:</span> {rating}
-            </p>
-            <div className="flex gap-4 mt-4">
+            <div className="flex flex-col">
+              <h2 className="text-5xl font-bold">{bookName}</h2>
+              <p className="text-[20px] border-gray-200 border-b py-4">
+                By : {author}
+              </p>
+              <p className="py-4 border-gray-200 border-b">{category}</p>
+              <p className="py-4 text-gray-600">
+                <span className="font-bold text-black">Review :</span> {review}
+              </p>
+              <p className="pb-4 border-gray-200 border-b">
+                <span className="pr-5 font-bold">Tag</span>
+                {tags.map((tag, index) => (
+                  <button
+                    key={index}
+                    className="btn btn-sm rounded-full px-6 border-none bg-green-50 mr-4 text-[#23BE0A] text-lg"
+                  >
+                    #{tag}
+                  </button>
+                ))}
+              </p>
+              <p className="pb-2 pt-4 text-gray-600">
+                <span className="font-bold text-black">Number of Pages:</span>{" "}
+                {totalPages}
+              </p>
+              <p className="py-2 text-gray-600">
+                <span className="font-bold text-black">Publisher:</span>{" "}
+                {publisher}
+              </p>
+              <p className="py-2 text-gray-600">
+                <span className="font-bold text-black">
+                  Year of Publishing:
+                </span>{" "}
+                {yearOfPublishing}
+              </p>
+              <p className="py-2 text-gray-600">
+                <span className="font-bold text-black">Rating:</span> {rating}
+              </p>
+            </div>
+            <div className="flex gap-4 mt-4 flex-grow">
               <button className="btn btn-outline">Read</button>
               <button className="btn bg-red-400 text-white">Wishlist</button>
             </div>
