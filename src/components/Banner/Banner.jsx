35a9b26fe8eeb380cca63bea2c5218255bf84fe1
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 11:39:13 +0600

    updated books.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import bannerImg from "../../assets/banner-book.png";
 const Banner = () => {
   return (
     <div>
-      <div className="hero min-h-screen">
+      <div className="hero min-h-screen bg-gray-50 h-[554px] mx-auto">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={bannerImg} className="max-w-sm rounded-lg" />
           <div>
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -4,7 +4,7 @@ import Navbar from "../Navbar/Navbar";
 
 const Root = () => {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto mt-6">
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
