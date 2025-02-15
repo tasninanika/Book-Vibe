466762ed0f921e266fa925264392c6c13a3af14d
anikatasnin <jarintasnin09@gmail.com>
2025-02-16 00:10:11 +0600

    updated utility.js

--- src/utility/addToDb.js
+++ src/utility/addToDb.js
@@ -1,11 +1,15 @@
 const getStoredReadList = () =>{
-
+    const storedListStr = localStorage.getItem('read-list');
+    if(storedListStr){
+        const storedList = JSON.parse(storedListStr);
+        return storedList;
+    }
 }
 
 const addToStoredReadList = (id) =>{
     const storedList = getStoredReadList();
     if(storedList.includes(id)){
-
+        console.log(id);
     }
     else{
         storedList.push(id);
