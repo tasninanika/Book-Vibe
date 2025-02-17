8c551dd9c47c879c3fcb3460f6ebb75e0366e73b
anikatasnin <jarintasnin09@gmail.com>
2025-02-17 23:56:03 +0600

    updated error.jsx

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,10 +1,12 @@
 const Error = () => {
   return (
-    <div className="text-center">
-      <h1 className="text-5xl items-center">Oops!</h1>
-      <p className="text-lg items-center">
-        Sorry, an unexpected error has occurred.
-      </p>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-5xl items-center mb-4">Oops!</h1>
+        <p className="text-lg items-center">
+          Sorry, an unexpected error has occurred.
+        </p>
+      </div>
     </div>
   );
 };
