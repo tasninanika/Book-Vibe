f6ea469eae89d720ef4956f38f0ad0a1953fe51d
anikatasnin <jarintasnin09@gmail.com>
2025-02-17 11:23:43 +0600

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Home from "./components/Home/Home";
 import Dashboard from "./components/Dashboard/Dashboard";
 import BookDetail from "./components/BookDetail/BookDetail";
 import ListedBooks from "./components/ListedBooks/ListedBooks";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 const router = createBrowserRouter([
   {
